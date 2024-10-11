6842a6051f312785ffbff57c8f79398f0fb2349a
pronan <pronan@loyola.edu>
2024-10-11 14:18:34 -0400

    initial comments

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-# Read the README.md
-# Read it again
-# Your code here
-# Delete these 4 lines of comments
+# Programmers: Paige and Zain
+# Course:  CS151, Dr. Zelalem Jembre Yalew
+# Due Date: 10/16/24
+# Lab Assignment: Lab5
+# Problem Statement: Build a simulation of an ATM, where users can view their balance, deposit, or withdraw
+# Data In: whether user wants to withdraw, deposit, view balance, or exit, how much money user wants to withdraw or
+# deposit, if user has a coupon
+# Data Out: account balance, and errors
+# Credits: class resources
