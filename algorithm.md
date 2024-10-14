0e2d56db7fd633894a5b66acaee77456f13501a3
paigeronan <pronan@loyola.edu>
2024-10-13 20:07:08 -0400

    algorithm and code

--- algorithm.md
+++ algorithm.md
@@ -1,28 +1,33 @@
 # Algorithm Document
 
-1. set initial balance to 1000
-2. ask user if they want to deposit withdraw, view balance, or exit 
-3. convert options to lowercase
+1. set balance to 1000
+2. output balance
 4. SENTINEL = 'e'
+5. set options to 'a'
 5. while options does not equal SENTINEL:
+   2. ask user if they want to deposit withdraw, view balance, or exit 
+   3. convert options to lowercase
    1. if options is equal to 'w':
       1. ask user how much they want to withdraw 
       2. if withdraw is less than 0:
-         1. output 'Error, cant input negative numbers try again'
-      3. otherwise
-         1. set new balance to the value of initial balance minus money withdraw
+         1. output 'Error, cant input negative numbers'
+      3. otherwise:
+         1. set new balance to the value of balance minus money withdraw
+         2. set balance equal to new balance
          2. output new balance 
       4. if new balance is less than 0:
          1. output 'warning'
    2. if otherwise options is equal to 'd':
       1. ask user how much they want to deposit
       2. if deposit is less than 0:
-         1. output 'Error, cant input negative numbers try again'
+         1. output 'Error, cant input negative numbers'
       3. otherwise
-         1. add deposit to initial balance
+         1. add deposit to balance
          2. output new balance 
    3. otherwise options equal 'v':
       1. output initial balance 
+   4. otherwise options = 'e':
+      5. options equals SENTINEL
    4. otherwise options does not equal 'w', 'd', 'v', or 'e':
       1. output'That is not a valid choice, enter 'w', 'd', 'v', or 'e''
 6. output'Thank you for using the atm'
--- main.py
+++ main.py
@@ -8,30 +8,46 @@
 # Data Out: account balance, and errors
 # Credits: class resources
 
-initial_balance = 1000
+balance = 1000
+print("Your initial balance is", balance)
 SENTINEL = 'e'
+options = 'a'
 new_balance = 0
 
 while options != SENTINEL:
-    options = input('Do you want to withdraw, deposit, view balance, or exit?')
+    options = input('Do you want to withdraw, deposit, view balance, or exit?\n'
+                    'W - Withdraw\n'
+                    'D - Deposit\n'
+                    'V - View Balance\n'
+                    'E - Exit\n')
     options = options.lower()
+
     if options == "w":
         withdraw_amount = int(input('Enter amount to withdraw: '))
         if withdraw_amount < 0:
-            print("Cant input negative amount, try again.")
+            print("Cant input negative amount.")
         else:
-            new_balance = initial_balance - withdraw_amount
+            new_balance = balance - withdraw_amount
+            balance = new_balance
             print('Your new balance is', new_balance)
         if new_balance < 0:
             print("Warning: You will be charged 5% interest for going under $0.")
+
     elif options == 'd':
         deposit_amount = int(input('Enter amount to deposit: '))
         if deposit_amount < 0:
             print("Cant input negative amount, try again.")
-        elif new_balance == deposit_amount + initial_balance:
-            print(new_balance)
+        else:
+            new_balance += deposit_amount
+            print("Your new balance is", new_balance)
+
     elif options == 'v':
-        print(new_balance)
+        print("Your balance is", new_balance)
+    elif options == 'e':
+        options = SENTINEL
+    elif options != 'w' and options != 'd' and options != 'v' and options != 'e':
+        print("That is not a valid option. Please enter 'w', 'd', 'v', or 'e'")
+print("Thank you for using this program!")
 
 
 
